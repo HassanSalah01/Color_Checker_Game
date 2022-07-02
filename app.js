86f5450600f7cfdb3446b0b3ae1568234bf6a7ba
Hassan-Salah <93399865+HassanSalah01@users.noreply.github.com>
2022-07-02 21:51:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -17,4 +17,41 @@ const colorPicker=(firstColor,secondColor,pick)=>{
     eve(firstColor,secondColor,pick);
 
 }
+const display=(firstColor,secondColor)=>{
+    if(firstColor!=secondColor){
+        cOne.style.backgroundColor = Colors[firstColor].getCol();
+        cTwo.style.backgroundColor = Colors[secondColor].getCol();
+    }else{
+        console.log("testing")
+        firstColor = rand();
+        cOne.style.backgroundColor = Colors[firstColor].getCol();
+        cTwo.style.backgroundColor = Colors[secondColor].getCol();
+    }
+    return [firstColor,secondColor]
+}
+
+const picks=(pick,firstColor,secondColor)=>{
+    if(pick>5){
+        colName.innerHTML = Colors[firstColor].getCol();
+    }else{
+        colName.innerHTML = Colors[secondColor].getCol();
+    }
+}
+
+const checkAns = (playerAnswer,rightAnswer)=>{
+    if(playerAnswer==rightAnswer){
+        console.log("Wrong")
+    }else{
+        console.log("RIGHT");
+    }
+}
+const eve = (firstColor , secondColor,pick)=>{
+    let picks = pick > 5 ? firstColor : secondColor;
+    cOne.addEventListener("mousedown", ()=>{
+        checkAns(Colors[firstColor].getCol(),Colors[picks].getCol())
+    });
+    cTwo.addEventListener("mousedown", ()=>{
+        checkAns(Colors[secondColor].getCol(),Colors[picks].getCol())
+    });
+}
 
